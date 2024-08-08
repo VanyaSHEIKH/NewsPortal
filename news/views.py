6fcc809d085b1726ac2d03a0ee4aff4cf1d1cf96
Ivan <itsvanyka@mail.ru>
2024-08-08 16:56:33 +0300

    < the sec commit>

--- news/views.py
+++ news/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
+from datetime import datetime
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = 'date_in'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-date_in')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['list_in_page'] = self.paginate_by
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+
