797b2fe59cdfab42609a27eddd7e4e93faff9cff
Ivan <itsvanyka@mail.ru>
2024-09-15 22:05:50 +0300

    celery

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -163,17 +163,17 @@ ACCOUNT_FORMS = {'signup': 'sign.models.BasicSignupForm'}
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'Ivan.Mikhov2004'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'ijlkajwqjrclaipw'  # пароль от почты
+EMAIL_HOST_USER = ''  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = ''  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
-DEFAULT_FROM_EMAIL = 'Ivan.Mikhov2004@yandex.ru'
+DEFAULT_FROM_EMAIL = ''
 
 MANAGERS = [
-    ('Skavik', 'itsvanyka@mail.ru'),
+    ('Skavik', ''),
     # список всех админов в формате ('имя', 'их почта')
 ]
-SERVER_EMAIL = 'Ivan.Mikhov2004@yandex.ru'  # это будет у нас вместо аргумента FROM в массовой рассылке
+SERVER_EMAIL = ''  # это будет у нас вместо аргумента FROM в массовой рассылке
 
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
